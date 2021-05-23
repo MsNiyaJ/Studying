ee8cd5fe164b491a8b96d0035da480c094272f6a
Shaniya <47842684+MsNiyaJ@users.noreply.github.com>
2021-05-23 01:57:59 -0400

    gets the key from an array

--- Algorithms & Data Structures/Arrays/Search.java
+++ Algorithms & Data Structures/Arrays/Search.java
@@ -3,22 +3,33 @@ import java.util.Arrays;
 public class Search {
     public static void main(String[] args) {
         int arr[] = new int[]{10, 2, 3, 15, 4, 5, 20, 17};
-        int key = 4;
-        binarySearch(arr, key);
+        int key = 1;
+        int index = binarySearch(arr, key);
+        
+        if(index != -1) System.out.println(key + " was found at index " + index + "!");
+        else System.out.println(key + " not found.");
     }
 
     /**
-     * Searches for a key in a sorted array.
+     * Searches for a key in a sorted array. (log n - # of passes)
      * @param arr The array we are searching,
      * @param key The key we are looking for in the array.
-     * @return index where the key was found or null if the key doesn't exist in the array.
+     * @return index where the key was found or -1 if the key doesn't exist in the array.
      */
-    private static void binarySearch(int[] arr, int key) {
+    private static int binarySearch(int[] arr, int key) {
         Arrays.sort(arr);   //Must sort the array before performing a binary search
         
         int min = 0;                //The smallest index
         int max = arr.length - 1;   //The highest index
-        int mid = (min + max) / 2;  //The midpoint of the array
         
+        //Makes the array smaller while searching for the key
+        while(min <= max){
+            int mid = (min + max) / 2;  //The midpoint of the array
+        
+            if(key < arr[mid]) max = mid - 1;        // Updates max if the key is on the left of the mid
+            else if(key > arr[mid]) min = mid + 1;   // Updates min if the key is on the left of the mid
+            else return mid;                         // Returns the mid when min, max, and mid equal each other
+        }
+        return -1;      // Returns -1 if the key wasn't found
     }
 }
