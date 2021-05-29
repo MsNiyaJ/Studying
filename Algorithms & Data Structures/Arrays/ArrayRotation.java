48dcecf99b161ad6369dbb893f3e5d47df4e4a07
Shaniya <47842684+MsNiyaJ@users.noreply.github.com>
2021-05-28 22:55:32 -0400

    rotates elements in an array by a given value

--- Algorithms & Data Structures/Arrays/ArrayRotation.java
+++ Algorithms & Data Structures/Arrays/ArrayRotation.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class ArrayRotation {
     public static void main(String[] args) {
         int[] arr = new int[]{1, 2, 3, 4, 5, 6};
@@ -11,9 +13,17 @@ public class ArrayRotation {
      * @param arr   The array we are rotating
      * @param d     The number of elements in the array
      * @param n     The size of the array
+     * @author      Shaniya Malcolm
      */
     public static void rotate(int[] arr, int d, int n) {
-        System.out.println("rotating the elements");
+        int[] rotatedArr = new int[n];  // Create a new array of the same size
+        
+        // For each element, increase the index by the size of d and take the modulo to get the first elements in the array 
+        for(int i = 0; i < n; i++){
+            rotatedArr[i] = arr[(i + d) % n];
+        }
+
+        System.out.println(Arrays.toString(rotatedArr));
     }
     
 }
