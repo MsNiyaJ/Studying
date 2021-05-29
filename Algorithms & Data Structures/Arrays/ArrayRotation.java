7e4602d12829bd97f7e87753a65c4767c4739494
Shaniya <47842684+MsNiyaJ@users.noreply.github.com>
2021-05-28 22:06:48 -0400

    Created method: rotate(arr, d, n)

--- Algorithms & Data Structures/Arrays/ArrayRotation.java
+++ Algorithms & Data Structures/Arrays/ArrayRotation.java
@@ -1,9 +1,19 @@
-import java.util.Arrays;
-
 public class ArrayRotation {
     public static void main(String[] args) {
         int[] arr = new int[]{1, 2, 3, 4, 5, 6};
-        // System.out.println(Arrays.toString(arr));
+        int n = arr.length;     //Size of the array
+        int d = 2;              //The number of elements we are rotating
+        rotate(arr, d, n);
+    }
+
+    /**
+     * Rotates an array of size n by d elements
+     * @param arr   The array we are rotating
+     * @param d     The number of elements in the array
+     * @param n     The size 
+     */
+    public static void rotate(int[] arr, int d, int n) {
+        System.out.println("rotating the elements");
     }
     
 }
