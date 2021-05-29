9563df527654dd2fcf454c075aab7a718515f2c3
Shaniya <47842684+MsNiyaJ@users.noreply.github.com>
2021-05-28 21:51:29 -0400

    binarySearch() prints the results of the search

--- Algorithms & Data Structures/Arrays/Search.java
+++ Algorithms & Data Structures/Arrays/Search.java
@@ -2,16 +2,19 @@ import java.util.Arrays;
 
 public class Search {
     public static void main(String[] args) {
+        startSearch();
+    }
+
+    private static void startSearch() {
         int arr[] = new int[]{10, 2, 3, 15, 4, 5, 20, 17};
-        int key = 4;
-        int index = binarySearch(arr, key);
-        print(index, key, "binary search");
+        int key = 7;
+        binarySearch(arr, key);
     }
 
     // Prints a message that shows if the key exists or not.
     private static void print(int index, int key, String search) {
         if(index != -1) System.out.println(key + " was found at index " + index + "! (" + search + ")");
-        else System.out.println(key + " not found.");
+        else System.out.println(key + " was not found.");
     }
 
     /**
@@ -20,20 +23,24 @@ public class Search {
      * @param key The key we are looking for in the array.
      * @return index where the key was found or -1 if the key doesn't exist in the array.
      */
-    private static int binarySearch(int[] arr, int key) {
+    private static void binarySearch(int[] arr, int key) {
         Arrays.sort(arr);   //Must sort the array before performing a binary search
         
         int min = 0;                //The smallest index
         int max = arr.length - 1;   //The highest index
-        
+        int index = -1;
+
         //Makes the array smaller while searching for the key
         while(min <= max){
             int mid = (min + max) / 2;  //The midpoint of the array
         
             if(key < arr[mid]) max = mid - 1;        // Updates max if the key is on the left of the mid
             else if(key > arr[mid]) min = mid + 1;   // Updates min if the key is on the left of the mid
-            else return mid;                         // Returns the mid when min, max, and mid equal each other
+            else {
+                index = mid;                         // If the key was found, store the index and end the while loop
+                break;
+            }
         }
-        return -1;      // Returns -1 if the key wasn't found
+        print(index, key, "binary search");         // Prints the result of the search
     }
 }
